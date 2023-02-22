baf6af5c409ee99defbb870c9de8c97abb842deb
kowerkoint <kowerkoint@gmail.com>
2023-02-22 18:35:34 +0900

    edit default config

--- config.py
+++ config.py
@@ -5,13 +5,19 @@ contest_sets = [
     {
         'name': 'asakatsu_extra',
         'title': 'あさかつ%-m/%-d EXTRA',
-        'memo': 'あさかつ公認のおまけ黄Diff（含：試験管）です。 感想戦も同じサーバーでやっています。',
+        'memo': 'あさかつ公認のおまけ黄Diff2問（含：試験管）です。 感想戦も同じサーバーでやっています。',
         'everyday_start_time': '07:30',
         'duration_second': 3600,
         'penalty_second': 300,
         'problem_infos': [
             {
-                'difficulty_range': (2000, 2399),
+                'difficulty_range': (2000, 2199),
+                'point': 1,
+                'include_experimental': True,
+                'duplicate_remove_days': 60
+            }
+            {
+                'difficulty_range': (2200, 2399),
                 'point': 1,
                 'include_experimental': True,
                 'duplicate_remove_days': 60
